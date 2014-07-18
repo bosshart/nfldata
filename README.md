1d3d46147515d41185b64c13eb11ac3246120c86
bosshart <cloudera@localhost.localdomain>
2014-07-18 08:04:48 -0700

    Added Tableau dashboard and additional fields to search.

--- README.md
+++ README.md
@@ -24,4 +24,13 @@ See the queries in queries.hql for some examples of how and what to query.
 
 Incomplete Passes
 =================
+
 Simple MapReduce on NFL play by play data.  This program focuses on incomplete passes and which receiver they were throw to.  See http://www.jesse-anderson.com/2013/01/nfl-play-by-play-analysis/ for the resulting charts.
+
+Play Search
+===========
+
+Based on merged dataset from above ETL Series. Data is parsed using Morphlines and indexed using MapReduceIndexerTool. 
+
+Setup the play search by editing & running the setupsearch.sh script. Script should work with no changes on quickstart VM. Otherwise you will need to fill env variables for ZK ensemble and base directories. 
+
--- setupsearch.sh
+++ setupsearch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-
-
-
-
 echo "Setting up env"
 export SOLR_ZK_ENSEMBLE=localhost:2181/solr
 export BASE_DIR=/home/cloudera/workspace/nfldata
 export PROJECT_HOME=$BASE_DIR/nflsearch
+export CLOUDERA_SEARCH_MR_PATH=/opt/cloudera/parcels/SOLR-1.1.0-1.cdh4.3.0.p0.21/lib/solr/contrib/mr/search-mr*-job.jar
+
+#/usr/lib/solr/contrib/mr/search-mr-1.0.0-job.jar 
+
+#/opt/cloudera/parcels/SOLR-1.1.0-1.cdh4.3.0.p0.21/lib/solr/contrib/mr/search-mr*-job.jar
 
 echo "Cleanup any old configs"
 {
@@ -24,6 +25,6 @@ cp $BASE_DIR/log4j.properties $PROJECT_HOME/log4j.properties
 solrctl --zk $SOLR_ZK_ENSEMBLE instancedir --create NFL-Collection $PROJECT_HOME
 solrctl --zk $SOLR_ZK_ENSEMBLE collection --create NFL-Collection -s 1
 
-#hadoop jar /opt/cloudera/parcels/SOLR-1.1.0-1.cdh4.3.0.p0.21/lib/solr/contrib/mr/search-mr*-job.jar org.apache.solr.hadoop.MapReduceIndexerTool -D 'mapred.child.java.opts=-Xmx500m' --log4j $PROJECT_HOME/log4j.properties --morphline-file $BASE_DIR/nflmorphlines.conf --output-dir hdfs://localhost:8020/user/cloudera/nflsearchdata/ --verbose --go-live --zk-host localhost:2181/solr --collection NFL-Collection --dry-run hdfs://localhost:8020/user/hive/warehouse/playbyplay/
+hadoop jar /opt/cloudera/parcels/SOLR-1.1.0-1.cdh4.3.0.p0.21/lib/solr/contrib/mr/search-mr*-job.jar org.apache.solr.hadoop.MapReduceIndexerTool -D 'mapred.child.java.opts=-Xmx500m' --log4j $PROJECT_HOME/log4j.properties --morphline-file $BASE_DIR/nflmorphlines.conf --output-dir hdfs://localhost:8020/user/cloudera/nflsearchdata/ --verbose --go-live --zk-host localhost:2181/solr --collection NFL-Collection --dry-run hdfs://localhost:8020/user/hive/warehouse/playbyplay/
 
-hadoop jar /opt/cloudera/parcels/SOLR-1.1.0-1.cdh4.3.0.p0.21/lib/solr/contrib/mr/search-mr*-job.jar org.apache.solr.hadoop.MapReduceIndexerTool -D 'mapred.child.java.opts=-Xmx500m' --log4j $PROJECT_HOME/log4j.properties --morphline-file $BASE_DIR/nflmorphlines.conf --output-dir hdfs://localhost:8020/user/cloudera/nflsearchdata/ --verbose --go-live --zk-host localhost:2181/solr --collection NFL-Collection hdfs://localhost:8020/user/hive/warehouse/playbyplay/
+#hadoop jar $CLOUDERA_SEARCH_MR_PATH org.apache.solr.hadoop.MapReduceIndexerTool -D 'mapred.child.java.opts=-Xmx500m' --log4j $PROJECT_HOME/log4j.properties --morphline-file $BASE_DIR/nflmorphlines.conf --output-dir hdfs:/user/cloudera/nflsearchdata/ --verbose --go-live --zk-host $SOLR_ZK_ENSEMBLE --collection NFL-Collection hdfs:/user/hive/warehouse/playbyplay/
